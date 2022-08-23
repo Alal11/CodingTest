6e0d2e70531645b1952e78fa5f443f4ec2bd8da2
Alal11 <104975763+Alal11@users.noreply.github.com>
2022-08-23 13:33:38 +0900

    윤년

---
+++ C/BOJ/2753.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int y;
+
+	scanf("%d", &y);
+
+	if (y % 4 == 0 && (y % 400 == 0 || y % 100 != 0))
+		printf("1");
+
+	else
+		printf("0");
+
+	return 0;
+}
+
+
+
+// 코드 간단화
+#include <stdio.h>
+
+int main(void)
+{
+	int y;
+
+	scanf("%d", &y);
+
+	printf("%d", y % 4 == 0 && (y % 400 == 0 || y % 100 != 0));
+
+	return 0;
+}
