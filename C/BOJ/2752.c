da8ea782cdaecafe0d38a26ff926fff09f591074
Alal11 <104975763+Alal11@users.noreply.github.com>
2022-08-23 14:07:47 +0900

    세수정렬

---
+++ C/BOJ/2752.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a, b, c;
+
+	scanf_s("%d %d %d", &a, &b, &c);
+
+	if (a < b && b < c) {
+		printf("%d %d %d", a, b, c);
+	}
+	else if (a < c && c < b) {
+		printf("%d %d %d", a, c, b);
+	}
+	else if (b < c && c < a) {
+		printf("%d %d %d", b, c, a);
+	}
+	else if (b < a && a < c) {
+		printf("%d %d %d", b, a, c);
+	}
+	else if (c < b && b < a) {
+		printf("%d %d %d", c, b, a);
+	}
+	else
+		printf("%d %d %d", c, a, b);
+
+	return 0;
+}
+
+
+// 다른 풀이
+#include <stdio.h>
+
+int main() {
+    int a, b, c, t;
+    scanf_s("%d %d %d", &a, &b, &c);
+    if (a > b) {
+        t = a; a = b; b = t;
+    }
+    if (b > c) {
+        t = b; b = c; c = t;
+    }
+    if (a > b) {
+        t = a; a = b; b = t;
+    }
+
+    printf("%d %d %d\n", a, b, c);
+    return 0;
+}
