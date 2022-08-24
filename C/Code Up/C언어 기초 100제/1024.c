63403b548d65dad071cb406f1cd6d12105bedef6
Alal11 <104975763+Alal11@users.noreply.github.com>
2022-08-24 21:55:43 +0900

    [기초-입출력] 단어 1개 입력받아 나누어 출력하기(설명)

---
+++ C/Code Up/C언어 기초 100제/1024.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i;
+	char word[21];
+
+	scanf("%s", word, sizeof(word));
+	for (i = 0; word[i]!= '\0'; i++)
+	{
+		printf("\'%c\'\n", word[i]);
+	}
+	return 0;
+}
