e3fd11358d9f7d122ce5c0d1d41eb6cf6d4ab0a8
Alal11 <104975763+Alal11@users.noreply.github.com>
2022-08-26 23:52:01 +0900

    벌집

---
+++ C/BOJ/2292.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n, i = 2, j = 5, cnt = 2;
+
+    scanf("%d", &n);
+
+    if (n == 1)             // 1인 경우 1을 출력
+    {
+        printf("%d", 1);
+        return 0;
+    }
+    while (1)               // 무한 반복문
+    {
+        if (i <= n && i + j >= n) 
+        {
+            printf("%d", cnt);
+            break;
+        }
+        i = i + j + 1;
+        j += 6;
+        cnt++;
+    }
+    return 0;
+}
