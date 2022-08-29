9895164da03d7721b8bfdfe029e73be00faa14c0
Alal11 <104975763+Alal11@users.noreply.github.com>
2022-08-30 00:43:32 +0900

    [기초-조건/선택실행구조] 정수 1개 입력받아 평가 출력하기(설명)

--- C/Code Up/C언어 기초 100제/1068.c
+++ C/Code Up/C언어 기초 100제/1068.c
@@ -2,17 +2,44 @@
 
 int main(void)
 {
-	int a;
+	int jumsu;
 
-	scanf("%d", &a);
+	scanf("%d", &jumsu);
 
-	if (a >= 90)
+	if (90 <= jumsu && 100 >= jumsu)
+		printf("A");
+
+	else if (70 <= jumsu && 89 >= jumsu)
+		printf("B");
+
+	else if (40 <= jumsu && 69 >= jumsu)
+		printf("C");
+
+	else if (0 <= jumsu && 39 >= jumsu)
+		printf("D");
+
+	return 0;
+}
+
+
+// 다른 풀이
+
+#include <stdio.h>
+
+int main(void)
+{
+	int jumsu;
+
+	scanf("%d", &jumsu);
+
+	if (jumsu >= 90)
 		printf("a");
-	else if (a >= 70)
+	else if (jumsu >= 70)
 		printf("b");
-	else if (a >= 40)
+	else if (jumsu >= 40)
 		printf("c");
-	else
+	else if (jumsu >= 0)
 		printf("d");
+
 	return 0;
 }
