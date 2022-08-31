825302cff8fb9d66e868f705d2424c7f9b565cfa
Alal11 <104975763+Alal11@users.noreply.github.com>
2022-08-31 19:56:53 +0900

    [중첩 반복문] 지그재그 출력하기

--- C/Code Up/기초 1~4/1370.c
+++ C/Code Up/기초 1~4/1370.c
@@ -3,12 +3,12 @@
 int main(void)
 {
 	int h, r;			          // 높이와 반복 횟수 변수 선언
-	int i, j, k;	        	// 삼중중첩문 활용 변수
+	int i, j, k;	        		  // 삼중중첩문 활용 변수
 
 	scanf("%d %d", &h, &r);
 
-	for (k = 1; k <= r; k++) {	       	// for문을 이용해 반복 횟수 만큼 반복
-		for (i = 1; i <= h; i++) {	    	// 별 계단 높이 증가
+	for (k = 1; k <= r; k++) {	       	    // for문을 이용해 반복 횟수 만큼 반복
+		for (i = 1; i <= h; i++) {	    // 별 계단 높이 증가
 			for (j = 1; j < i; j++) {
 				printf(" ");
 			}
