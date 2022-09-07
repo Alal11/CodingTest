64d79df4733e9a6a0b312e6e443fcdc455cb4778
Alal11 <104975763+Alal11@users.noreply.github.com>
2022-09-08 01:16:08 +0900

    B5 [구현]

--- C/BOJ/BRONZE/10926_??!.c
+++ C/BOJ/BRONZE/10926_??!.c
@@ -4,9 +4,9 @@ int main()
 {
 	char a[51];
 
-	scanf("%s", a, sizeof(a));       // 문자열 입력 받을땐 & 생략
+	scanf("%s", a);			// 문자열 입력 받을땐 & 생략
 
-	printf("%s??!\n", a);
+	printf("%s?\?!\n", a);
 
 	return 0;
 }
