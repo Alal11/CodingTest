02ddf5a920d00c9f285e93b67d2d432ca4eae1a7
Alal11 <104975763+Alal11@users.noreply.github.com>
2022-11-03 17:48:15 +0900

    S5 [정렬]

---
+++ Python/BOJ/SILVER/2628_종이자르기.py
@@ -0,0 +1,26 @@
+x, y = map(int, input().split())
+
+gr = [x, 0]
+sr = [0, y]
+
+line = int(input())
+
+for i in range(line):
+    n, m = map(int, input().split())
+
+    if n == 0:
+        sr.append(m)
+    else:
+        gr.append(m
+
+gr = sorted(gr)
+sr = sorted(sr)
+result_list = []
+
+for i in range(1, len(gr)):
+    for j in range(1, len(sr)):
+        w = gr[i]-gr[i-1]
+        h = sr[j]-sr[j-1]
+        result_list.append(w*h)
+
+print(max(result_list))
