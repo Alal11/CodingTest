d39fbf2ddfc830eb37653724aa7cf46ba47bd3de
Alal11 <104975763+Alal11@users.noreply.github.com>
2022-11-24 14:02:35 +0900

    B2 [수학, 구현, 시뮬레이션]

---
+++ Python/BOJ/BRONZE/4998_저금.py
@@ -0,0 +1,12 @@
+try:
+    while (1):
+        n, b, m = map(float, input().split())
+        cnt = 0
+        while (n < m):
+            n += n*(b/100)
+            cnt += 1
+
+        print(cnt)
+
+except:
+    exit()
