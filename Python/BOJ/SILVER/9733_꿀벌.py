0ca1a462fed7a0f08ee6d20184112ca6a4ba0fb9
Alal11 <104975763+Alal11@users.noreply.github.com>
2022-12-05 11:28:29 +0900

    S5 [구현, 자료 구조, 문자열, 해시를 사용한 집합과 맵]

---
+++ Python/BOJ/SILVER/9733_꿀벌.py
@@ -0,0 +1,23 @@
+import sys
+
+work = ['Re', 'Pt', 'Cc', 'Ea', 'Tb', 'Cm', 'Ex']
+
+bee = dict()
+for i in work:
+    bee[i] = 0
+
+cnt = 0
+line = sys.stdin.readlines()
+
+for i in line:
+    str = list(i.split())
+    
+    for j in str:
+        if j in work:
+            bee[j] = bee.get(j, 0)+1
+        cnt += 1
+
+for i in bee:
+    print(i, bee.get(i), '{:.2f}'.format(bee.get(i)/cnt))
+
+print('Total', cnt, '1.00')
