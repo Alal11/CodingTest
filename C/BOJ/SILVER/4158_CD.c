6cc34f947a3f4a14cca5e31ad6fbd07621b0a352
Alal11 <104975763+Alal11@users.noreply.github.com>
2023-03-12 19:14:08 +0900

    S5 [자료 구조, 이분 탐색, 해시를 사용한 집합과 맵, 두 포인터]

---
+++ C/BOJ/SILVER/4158_CD.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	while (1)
+	{
+		int n, m;
+		
+		scanf("%d %d", &n, &m);
+
+		if (n == 0 && m == 0)
+			break;
+
+		int* arr1 = (int*)malloc(sizeof(int)*n);
+		int* arr2 = (int*)malloc(sizeof(int)*m);
+
+		for (int i = 0; i < n; i++)
+		{
+			scanf("%d", &arr1[i]);
+		}
+		for (int i = 0; i < m; i++)
+		{
+			scanf("%d", &arr2[i]);
+		}
+
+		int ptr1 = 0, ptr2 = 0;
+		int cnt = 0;
+
+		while (ptr1 < n && ptr2 < m)
+		{
+			if (arr1[ptr1] < arr2[ptr2])
+				ptr1++;
+			else if (arr1[ptr1] > arr2[ptr2])
+				ptr2++;
+			else
+			{
+				cnt++;
+				ptr1++;
+				ptr2++;
+			}
+		}
+		printf("%d\n", cnt);
+
+		free(arr1);
+		free(arr2);
+	}
+	return 0;
+}
