b24950232cd6622ad0ded15b1d893b951b7e4f2d
Alal11 <104975763+Alal11@users.noreply.github.com>
2023-03-16 13:44:50 +0900

    S3 [수학, 정수론, 소수 판정, 에라토스테네스의 체]

---
+++ Java/BOJ/Silver/1929_소수 구하기.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        int m=sc.nextInt();
+        int n= sc.nextInt();
+
+        int[] arr = new int[n+1];
+
+        for(int i=2; i<=n; i++)
+        {
+            arr[i]=i;
+        }
+        for(int i=2; i<=n; i++)
+        {
+            if(arr[i]==0) continue;
+
+            for(int j=i*2; j<=n; j+=i)
+            {
+              arr[j]=0;
+            }
+        }
+        for(int k=m; k<=n; k++)
+        {
+            if(arr[k]!=0)
+                System.out.println(arr[k]);
+        }
+    }
+}
