0d2038c23266e6b2e2c0edd5396bf61dfcf0e9dc
Alal11 <spalqj56@naver.com>
2023-07-30 14:55:09 +0900

    .

---
+++ Java/BOJ/Bronze/BOJ_3052.java
@@ -0,0 +1,29 @@
+import java.io.*;
+
+public class BOJ_3052 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int arr[] = new int[10];
+        for (int i = 0; i < 10; i++) {
+            arr[i] = Integer.parseInt(br.readLine()) % 42;
+        }
+
+        int result = 0;
+
+
+        for (int i = 0; i < 10; i++) {
+            Boolean cnt = true;
+            for (int j = i + 1; j < 10; j++) {
+                if (arr[i] == arr[j]) {
+                    cnt = false;
+                    break;
+                }
+            }
+            if (cnt == true) {
+                result++;
+            }
+        }
+        System.out.println(result);
+    }
+}
