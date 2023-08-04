04cb09fcdf30fdb16ad2ccb61d1236495bfa69b2
Alal11 <spalqj56@naver.com>
2023-08-04 09:09:15 +0900

    substring, switch

--- Java/BOJ/Bronze/BOJ_2754.java
+++ Java/BOJ/Bronze/BOJ_2754.java
@@ -11,30 +11,16 @@ public class BOJ_2754 {
 
         double result=0.0;
 
-        switch(c)
-        {
-            case "+":
-                result+=0.3;
-                break;
-            case "-":
-                result-=0.3;
-                break;
+        switch (c) {
+            case "+" -> result += 0.3;
+            case "-" -> result -= 0.3;
         }
 
-        switch(b)
-        {
-            case "A":
-                result+=4.0;
-                break;
-            case "B":
-                result+=3.0;
-                break;
-            case "C":
-                result+=2.0;
-                break;
-            case "D":
-                result+=1.0;
-                break;
+        switch (b) {
+            case "A" -> result += 4.0;
+            case "B" -> result += 3.0;
+            case "C" -> result += 2.0;
+            case "D" -> result += 1.0;
         }
 
         System.out.println(result);
