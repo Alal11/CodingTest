52d6e6043a0506aefb1b8af12a1905e59e0b7ea2
Alal11 <104975763+Alal11@users.noreply.github.com>
2024-09-02 22:43:33 +0900

    str 문자열을 반대로 뒤집어라. StringBuilder에 있는 reverse()를 사용해라.

---
+++ Java/algorithm study/M1_3.java
@@ -0,0 +1,10 @@
+public class M1_3 {
+    public static void main(String[] args) {
+        String src = "Hello Java";
+
+        StringBuilder sb = new StringBuilder();
+        System.out.println(sb.append(src).reverse());
+    }
+}
+
+// str 문자열을 반대로 뒤집어라. StringBuilder에 있는 reverse()를 사용해라.
