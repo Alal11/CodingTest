b55c7d77622af9a3ba2388ad6d659a0e3b630a17
Alal11 <104975763+Alal11@users.noreply.github.com>
2025-01-17 18:57:33 +0900

    .

---
+++ Java/BOJ/Bronze/BOJ_3009.java
@@ -0,0 +1,42 @@
+package bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_3009 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] x = new int[3];
+        int[] y = new int[3];
+
+        for (int i = 0; i < 3; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            x[i] = Integer.parseInt(st.nextToken());
+            y[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int xx = 0;
+        int yy = 0;
+
+        if (x[0] == x[1]) {
+            xx = x[2];
+        } else if (x[0] == x[2]) {
+            xx = x[1];
+        } else {
+            xx = x[0];
+        }
+
+        if (y[0] == y[1]) {
+            yy = y[2];
+        } else if (y[0] == y[2]) {
+            yy = y[1];
+        } else {
+            yy = y[0];
+        }
+
+        System.out.println(xx + " " + yy);
+    }
+}
