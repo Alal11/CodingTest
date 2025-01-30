889f510db6eb69f23251fd6a83b35e0c2d681a5e
Alal11 <104975763+Alal11@users.noreply.github.com>
2025-01-30 18:17:46 +0900

    정렬, 자료구조

---
+++ Java/BOJ/Silver/BOJ_11652.java
@@ -0,0 +1,41 @@
+package silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class BOJ_11652 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        long[] arr = new long[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Long.parseLong(br.readLine());
+        }
+
+        Arrays.sort(arr);
+
+        int cnt = 1;
+        int maxCnt = 1;
+        int maxIndex = 0;
+
+        for (int i = 1; i < n; i++) {
+            if (arr[i] == arr[i - 1]) {
+                cnt++;
+            } else {
+                cnt = 1;
+            }
+
+            if (cnt > maxCnt) {
+                maxCnt = cnt;
+                maxIndex = i;
+            }
+        }
+
+        System.out.println(arr[maxIndex]);
+    }
+}
