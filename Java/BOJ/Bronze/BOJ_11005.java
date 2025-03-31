31342ebd6eb9eb8d5f4d4d43052dd420dea18fa6
Alal11 <104975763+Alal11@users.noreply.github.com>
2025-03-31 23:54:41 +0900

    수학, 구현

---
+++ Java/BOJ/Bronze/BOJ_11005.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_11005 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int num = Integer.parseInt(st.nextToken());
+        int jb = Integer.parseInt(st.nextToken());
+
+        while (num >= 1) {
+            int k = num % jb;
+
+            if (k >= 10 && k <= 35) {
+                k += 55;
+                sb.append((char) k);
+            } else {
+                sb.append(k);
+            }
+            num /= jb;
+        }
+        System.out.println(sb.reverse());
+    }
+}
