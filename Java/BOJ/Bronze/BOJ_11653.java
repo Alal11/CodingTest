a5cbceb2113ec6f8065ffb2d2089f3b18e1a7036
Alal11 <104975763+Alal11@users.noreply.github.com>
2025-08-30 08:15:24 +0900

    정수론, 소인수분해

---
+++ Java/BOJ/Bronze/BOJ_11653.java
@@ -0,0 +1,24 @@
+package bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_11653 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 2; i <= n; i++) {
+            while (true) {
+                if (n % i == 0) {
+                    n = n / i;
+                    System.out.println(i);
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+}
